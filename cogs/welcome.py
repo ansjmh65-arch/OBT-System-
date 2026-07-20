0cc438f929ef5e404c24dab36667c6a707eec4b1
ansjmh65-arch <ansjmh65@gmail.com>
2026-07-20 18:06:48 +0300

    Add welcome cog for new members in Discord

---
+++ cogs/welcome.py
@@ -0,0 +1,16 @@
+import discord
+from discord.ext import commands
+
+class Welcome(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.Cog.listener()
+    async def on_member_join(self, member):
+        channel = member.guild.system_channel
+        if channel:
+            await channel.send(f"أهلاً بك يا {member.mention} في سيرفرنا! نورتنا.")
+
+async def setup(bot):
+    await bot.add_cog(Welcome(bot))
+  
