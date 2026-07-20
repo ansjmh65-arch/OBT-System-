246c7105b1cebdccf2aa5332b33c9f7add30d65a
ansjmh65-arch <ansjmh65@gmail.com>
2026-07-20 17:59:24 +0300

    Add SimpleCache class for basic caching functionality

---
+++ utils/cache.py
@@ -0,0 +1,11 @@
+class SimpleCache:
+    def __init__(self):
+        self._store = {}
+
+    def get(self, key):
+        return self._store.get(key)
+
+    def set(self, key, value):
+        self._store[key] = value
+
+cache = SimpleCache()
