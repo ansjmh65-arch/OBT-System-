2d28440cde9fc62c34949278dfac06ef0ffd3283
ansjmh65-arch <ansjmh65@gmail.com>
2026-07-21 05:34:25 +0300

    Update database.py

--- database/database.py
+++ database/database.py
@@ -1,9 +1,14 @@
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import event
+from sqlalchemy.engine import Engine
 
 db = SQLAlchemy()
 
-def init_db(app):
-    db.init_app(app)
-    with app.app_context():
-        db.create_all()
-      
+# تفعيل التحقق من قيود المفاتيح الأجنبية خصيصاً لـ SQLite
+@event.listens_for(Engine, "connect")
+def set_sqlite_pragma(dbapi_connection, connection_record):
+    if dbapi_connection.__class__.__name__ == "Connection": # تأكد أنه اتصال SQLite
+        cursor = dbapi_connection.cursor()
+        cursor.execute("PRAGMA foreign_keys=ON")
+        cursor.close()
+        
