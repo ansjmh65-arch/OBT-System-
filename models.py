c8e511c9bfd55a203a080626f8d2c0141a66302d
ansjmh65-arch <ansjmh65@gmail.com>
2026-07-21 05:25:03 +0300

    Update models.py

--- models.py
+++ models.py
@@ -7,24 +7,10 @@ class ServerConfig(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     guild_id = db.Column(db.String(50), unique=True, nullable=False, default="default_guild")
     
-    # حالة الأقسام الـ 14 (تفعيل / تعطيل)
+    # حالات الأقسام الأساسية
     security_enabled = db.Column(db.Boolean, default=True)
     moderation_enabled = db.Column(db.Boolean, default=True)
     tickets_enabled = db.Column(db.Boolean, default=True)
-    clans_enabled = db.Column(db.Boolean, default=True)
     economy_enabled = db.Column(db.Boolean, default=True)
-    content_creators_enabled = db.Column(db.Boolean, default=True)
     welcome_enabled = db.Column(db.Boolean, default=True)
-    logs_enabled = db.Column(db.Boolean, default=True)
-    auto_roles_enabled = db.Column(db.Boolean, default=True)
-    backup_enabled = db.Column(db.Boolean, default=True)
-    server_settings_enabled = db.Column(db.Boolean, default=True)
-    commands_management_enabled = db.Column(db.Boolean, default=True)
-    profile_enabled = db.Column(db.Boolean, default=True)
-    home_enabled = db.Column(db.Boolean, default=True)
-
-    # حقول إضافية للإعدادات
-    welcome_message = db.Column(db.Text, nullable=True)
-    welcome_channel = db.Column(db.String(50), nullable=True)
-    logs_channel = db.Column(db.String(50), nullable=True)
     
